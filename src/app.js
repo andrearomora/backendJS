1d2b8385f198c194d0647707bddb6809afb7c5e3
andrearomora <andreaesromora@gmail.com>
2023-07-17 23:43:09 -0500

    handlebars

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import productRouter from './routes/product.router.js'
 import cartRouter from './routes/cart.router.js'
+import viewsRouter from './routes/views.router.js'
+import handlebars from 'express-handlebars'
+import __dirname from './utils.js'
 
 const app = express()
 app.use(express.json())
 
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
+
+app.use('/', viewsRouter)
 app.use('/api/products', productRouter)
 app.use('/api/carts', cartRouter)
 
--- src/routes/cart.router.js
+++ src/routes/cart.router.js
@@ -18,11 +18,19 @@ router.get('/:cid', async (req, res) => {
 router.get('/:idc', async (req, res) => {
     const idc = parseInt(req.params.idc)
 
-    const result = await cartManager.getCartById(idc)
+    const result = await cartManager.getProductById(idc)
     res.send(result)
 })
 
 router.post('/', async (req, res) => {
+    const result = await cartManager.create()
+    res.send(result)
+})
+
+router.post('/:cid/product/:pid', async (req, res) => {
+    const cid = parseInt(req.params.cid)
+    const pid = parseInt(req.params.pid)
+
     const result = await cartManager.create()
     res.send(result)
 })
